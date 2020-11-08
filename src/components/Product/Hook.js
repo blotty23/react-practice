41d64acef94d479ae3d8c09801b4c1f0d1c21ca9
Emma Blott <blott23@gmail.com>
2020-11-08 18:47:25 +0000

    add state variables with hooks

--- src/components/Product/Hook.js
+++ src/components/Product/Hook.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
 import './Hook.css';
 
+const currencyOptions = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+}
+
 export default function Product() {
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  function getTotal() {
+    return total.toLocaleString(undefined,currencyOptions)
+  }
+
   return(
     <div className="wrapper">
       <div>
-        Shopping Cart: 0 total items.
+        Shopping Cart: {cart.length} total items.
       </div>
       <div>Total: 0</div>
 
-      <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
+      <div className="product"><span ro
+      le="img" aria-label="ice cream">🍦</span></div>
       <button>Add</button> <button>Remove</button>
     </div>
   )
