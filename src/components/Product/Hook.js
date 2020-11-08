e61a2ad303be62edc2422fcb384dbd1f3cb69671
Emma Blott <blott23@gmail.com>
2020-11-08 19:24:55 +0000

    create remove method using actions

--- src/components/Product/Hook.js
+++ src/components/Product/Hook.js
@@ -27,8 +27,17 @@ const currencyOptions = {
 function getTotal(total) {
   return total.toLocaleString(undefined,currencyOptions)
 }
-function cartReducer(state,product) {
-  return [...state, product]
+function cartReducer(state,action) {
+  switch(action.type) {
+    case 'add':
+      return [...state, action.name];
+    case 'remove':
+      const update = [...state];
+      update.splice(update.indexOf(action.name), 1);
+      return update;
+    default:
+      return state;
+  }
 }
 function totalReducer(state, action) {
   if(action.type === 'add') {
@@ -43,8 +52,13 @@ export default function Product() {
 
   function add(product) {
     const {name, price} = product
-    setCart(name);
+    setCart({name, type: 'add'});
     setTotal({price, type: 'add'});
+  }
+  function remove(product) {
+    const {name, price} = product
+    setCart({name, type: 'remove'});
+    setTotal({price, type: 'remove'});
   }
 
   return(
@@ -58,7 +72,7 @@ export default function Product() {
           <div key={product.name}>
             <div className="product"><span role="img" aria-label={product.name}>{product.emoji}</span></div>
             <button onClick={() => add(product)}>Add</button> 
-            <button>Remove</button>
+            <button onClick={() => remove(product)}>Remove</button>
           </div>
         ))}
       </div>
